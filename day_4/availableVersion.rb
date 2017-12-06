60414411bb9537897142389083badad75fb53a97
hwzss <1833361588@qq.com>
2017-12-06 22:26:19 +0800

    done

--- day_4/availableVersion.rb
+++ day_4/availableVersion.rb
@@ -1,6 +1,10 @@
 
-def availableVersion(version)
+def availableVersion(version_number)
 
-	
-	
+ 	unless version_number =~ /^\d{1,}.\d.\d$/
+ 		puts "version_number格式不对，正确格式应该为 A.B.C "
+ 		return false
+ 	end
+	# version_arrs = version_number.split(".").map{&:to_i}	
+	true	
 end
