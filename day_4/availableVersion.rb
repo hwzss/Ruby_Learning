54d9a39d8c8e68a7aeccd2a82d578170d64cf90a
hwzss <1833361588@qq.com>
2017-12-07 15:01:24 +0800

    day5完成自增函数

--- day_4/availableVersion.rb
+++ day_4/availableVersion.rb
@@ -9,6 +9,7 @@ def availableVersion(version_number)
 	true	
 end
 
+# 满足／^\d{1,}*／的值格式成 A.B.C的形式
 def fixVersionNumber(version_number)
 
 	version_arrs = version_number.split('.')
@@ -20,24 +21,52 @@ def fixVersionNumber(version_number)
 
 
 end
+
+# 将满足正则/^\d{1,}.\d.\d$/ 的值进行自增1
 def addVersionNumber(version_number)
 
 	version_arrs = version_number.split('.').map(&:to_i)
 
-
-	if version_arrs[2]>8
-		if version_arrs[1]>8
-			version_arrs[0] +=1
-			version_arrs[1] = 0
-			version_arrs[2] = 0
-		else
-			version_arrs[2] = 0
-			version_arrs[1] +=1
-		end
+	if version_number =~ /^\d{1,}.[0-8].9$/
+		 # puts "1"
+		 version_arrs[1] = version_arrs[1] + 1
+		 version_arrs[2] = 0
+	
+	elsif version_number =~ /^\d{1,}.9.9$/
+		 # puts "2"
+		 version_arrs[0] = version_arrs[0] + 1
+		 version_arrs[1] = 0
+		 version_arrs[2] = 0
 	else
-		version_arrs[2] +=1
+		 # puts "3"
+         version_arrs[2] = version_arrs[2] + 1
 	end
+
+	#上面的if语句是下面注释代码的改写版
+	# if version_arrs[2]>8
+	# 	if version_arrs[1]>8
+	# 		version_arrs[0] +=1
+	# 		version_arrs[1] = 0
+	# 		version_arrs[2] = 0
+	# 	else
+	# 		version_arrs[2] = 0
+	# 		version_arrs[1] +=1
+	# 	end
+	# else
+	# 	version_arrs[2] +=1
+	# end
 
 	version_arrs[0..2].join('.')
 
+end
+
+def  testBool
+
+	puts "0代表真" if 0
+	puts "0代表假" unless 0
+	puts "nil 代表真" if nil
+	puts "nil 代表假" unless nil
+
+	puts "true 代表假" unless true
+	puts "true 代表真" if false
 end
